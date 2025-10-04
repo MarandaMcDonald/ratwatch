6a89ed710b69f598519d904b6c7adb0866096887
manbearbird <tajweird@gmail.com>
2025-10-04 18:21:32 -0400

    index, first post

---
+++ styleandscripts/content-objects.js
@@ -0,0 +1,37 @@
+const contentObject = [
+  {
+    "image": "BikeKillJousting.jpg",
+    "title": "Tall Bike Jousting!",
+    "url": "https://www.tajweird.com/TallBikeJousting/",
+    "type": "Activity",
+    "year": "2023/2024/2025",
+    "tags": [
+      "Bikes",
+      "Events"
+    ],
+    "xPos": 0.5,
+    "yPos": 0.4
+  },
+  {
+    "image": ".jpg",
+    "title": "",
+    "url": "https://www.tajweird.com/oneEuro/",
+    "type": "",
+    "year": "",
+    "tags": [
+      ""
+    ],
+    "xPos": 0.52,
+    "yPos": 0.39
+  },
+  {
+    "image": "86.jpg",
+    "title": "Trigon",
+    "url": "https://www.tajweird.com/project/86/trigon",
+    "type": "Shortfilm",
+    "year": "1998",
+    "tags": [],
+    "xPos": 0.2,
+    "yPos": 0.55
+  }
+];
